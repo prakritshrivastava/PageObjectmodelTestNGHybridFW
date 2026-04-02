afe20361b7e00c44eee00549c3cf2c1307aaff5e
Prakrit Shrivastava <shrivastavaprakrit@gmail.com>
2026-04-02 07:26:14 +0530

    mypage updated

--- src/main/java/com/opencart/qa/pages/MyPage.java
+++ src/main/java/com/opencart/qa/pages/MyPage.java
@@ -2,8 +2,16 @@ package com.opencart.qa.pages;
 
 public class MyPage {
 
-	public static void main(String[] args) {	
-		int j=40;
-		System.out.println("Value is:"+j);
-	}	
+
+	public static void main(String[] args) {
+		
+		int i=20;
+		System.out.println("Value is:"+i);
+		
+		
+	}
+
+	
+	
+
 }
